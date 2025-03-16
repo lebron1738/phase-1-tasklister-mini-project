e8e7ca2d81c88830b0389e8d2dc7b4b814a96624
James Kuria <jameskuria423@gmail.com>
2025-03-16 19:37:26 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function (deed) {
+    deed.preventDefault();
+
+    const newTaskDescription = document.getElementById("new-task-description");
+    if (newTaskDescription.value.trim() !== "") {
+      const newTask = document.createElement("li");
+      newTask.innerText = newTaskDescription.value;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "x";
+      deleteButton.addEventListener("click", () => {
+        taskList.removeChild(newTask);
+      });
+      newTask.appendChild(deleteButton);
+
+      taskList.appendChild(newTask);
+      newTaskDescription.value = "";
+    }
+  });
 });
